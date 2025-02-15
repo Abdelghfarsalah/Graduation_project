ed9625cb04315ecd2e8543f727b7ce7fe1a5a64c
AbdelghfarKhairallah1 <140082083+Abdelghfarsalah@users.noreply.github.com>
2025-02-15 12:31:38 +0200

    Trending tracks card ui

--- lib/feature/home/presentation/Widgets/TrendingTracks.dart
+++ lib/feature/home/presentation/Widgets/TrendingTracks.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:graduation_project/feature/home/presentation/Widgets/TrendingTracksCard.dart';
 import 'package:graduation_project/feature/home/presentation/Widgets/TrendingtracksHeader.dart';
 
 class Trendingtracks extends StatelessWidget {
@@ -9,6 +10,72 @@ class Trendingtracks extends StatelessWidget {
     return Column(
       children: [
         Trendingtracksheader(),
+        SizedBox(
+          height: 30,
+        ),
+        Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Trendingtrackscard(
+                  padding: 15,
+                  height: 202,
+                  width: MediaQuery.sizeOf(context).width * 0.43,
+                  image: 'assets/HomeImage/trendingTracksImages/Cs.png',
+                  title: 'Computer Science',
+                  color: Color(0xffFFB4B4).withOpacity(0.5),
+                  top: false,
+                  subtitle: 'Software Engineering',
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Trendingtrackscard(
+                    padding: 8,
+                    height: 163,
+                    width: MediaQuery.sizeOf(context).width * 0.43 - 8,
+                    image: 'assets/HomeImage/trendingTracksImages/OR.png',
+                    title: ' Operations Research &Design Support',
+                    color: Color(0xffFFB1F3).withOpacity(0.5),
+                    top: true,
+                    subtitle: 'Data Analysis',
+                  ),
+                )
+              ],
+            ),
+            Spacer(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Trendingtrackscard(
+                  padding: 8,
+                  height: 163,
+                  width: MediaQuery.sizeOf(context).width * 0.43 - 8,
+                  image:
+                      'assets/HomeImage/trendingTracksImages/informationTech.png',
+                  title: ' Information Technology',
+                  color: Color(0xffB4FFE4).withOpacity(0.5),
+                  top: true,
+                  subtitle: 'Computer Networks',
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: Trendingtrackscard(
+                    padding: 15,
+                    height: 202,
+                    width: MediaQuery.sizeOf(context).width * 0.43,
+                    image:
+                        'assets/HomeImage/trendingTracksImages/ArtificialIntelligence.png',
+                    title: 'Artificial Intelligence',
+                    color: Color(0xffFF6348).withOpacity(0.5),
+                    top: false,
+                    subtitle: 'Machine Learning',
+                  ),
+                ),
+              ],
+            )
+          ],
+        )
       ],
     );
   }
---
+++ lib/feature/home/presentation/Widgets/TrendingTracksCard.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_project/core/fonts.dart';
+
+class Trendingtrackscard extends StatelessWidget {
+  const Trendingtrackscard(
+      {super.key,
+      required this.image,
+      required this.title,
+      required this.color,
+      required this.top,
+      required this.subtitle,
+      required this.height,
+      required this.width,
+      required this.padding});
+  final String image;
+  final String title;
+  final String subtitle;
+  final Color color;
+  final bool top;
+  final double height;
+  final double width;
+  final double padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(24), color: Color(0xffF8F7FF)),
+      child: Padding(
+        padding: EdgeInsets.all(padding),
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24), color: color),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              if (top) Image.asset(image),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                    child: Text(
+                      title,
+                      textAlign: TextAlign.center,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xff0C0A1C),
+                          fontFamily: appFonts.Poppins),
+                    ),
+                  ),
+                  Text(
+                    subtitle,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.grey,
+                        fontFamily: appFonts.Poppins),
+                  ),
+                ],
+              ),
+              if (!top) Image.asset(image),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
